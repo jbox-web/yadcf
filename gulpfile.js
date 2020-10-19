7f5e5bb2c9072b7e3ebc88e97ce345a343840c6c
Nicolas Rodriguez <nicolas.rodriguez@digitick.com>
2020-10-19 12:24:34 +0200

    Drop gulp-coffeelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,9 @@
-const gulp       = require('gulp');
-const coffee     = require('gulp-coffee');
-const coffeelint = require('gulp-coffeelint');
-const eslint     = require('gulp-eslint');
+const gulp   = require('gulp');
+const coffee = require('gulp-coffee');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', () =>
   gulp.src('src/yadcf.coffee')
-    .pipe(coffeelint())
-    .pipe(coffeelint.reporter())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
